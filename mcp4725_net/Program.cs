b2c590965f2a0c0efbe5ed2bfe60356a502c05ef
Dave <bigmanrunning@gmail.com>
2020-08-01 14:55:26 -0500

    intermediate 2

---
+++ mcp4725_net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mcp4725_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
